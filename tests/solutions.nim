27d6edae2825282066fdd47eed320d00283696d3
ZoomRmc <zoomrmc@gmail.com>
2020-12-07 18:02:54 +0300

    Add basic tests, checking the output

---
+++ tests/solutions.nim
@@ -0,0 +1,10 @@
+import osproc
+
+doAssert execProcess("./aoc01") == "1005459\n92643264\n"
+doAssert execProcess("./aoc02") == "393\n690\n"
+doAssert execProcess("./aoc03") == "85\n3952146825\n"
+doAssert execProcess("./aoc04") == "210\n131\n"
+doAssert execProcess("./aoc05") == "965\n524\n"
+doAssert execProcess("./aoc06") == "6714\n3435\n"
+doAssert execProcess("./aoc07") == "161\n30899\n"
+
