8e17fe31b68c05828b83586e659f24c6a7b9e2cc
ZoomRmc <zoomrmc@gmail.com>
2020-12-12 18:44:27 +0300

    Update for Day 11: shorter and slower scanning

--- src/aoc11.nim
+++ src/aoc11.nim
@@ -13,11 +13,13 @@ LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL"""
 
+  DIRS = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+
 type  
   Cell = enum 
     Occ = "#",
     Floor = ".",
-    Empty = "L",
+    Empty = "L"
 
   Part = enum 
     P1, P2
@@ -25,54 +27,23 @@ type
 func parseRow(row: sink string): seq[Cell] =
   row.mapIt(parseEnum[Cell]($it))
 
+# Handling it this way relies on compiler to optimize out zero summations.
+# Range checking is probably suffers too, relative to manually unrolled branching.
+# However, this is shorter. Unrolled version's in git, if optimizations are needed.
 iterator scanAround(m: openArray[seq[Cell]]; r, c: int): Cell =
-  var
-    registered: seq[Cell]
-    x = c
-    y = r
-  template register(x, y: var int) =
-    if m[y][x] != Floor:
-      registered.add(m[y][x])
-      break
-  while y > 0: # N
-    y.dec()
-    register(x, y)
-  (x, y) = (c, r)
-  while y > 0 and x < m[0].len - 1: # NE
-    y.dec()
-    x.inc()
-    register(x, y)
-  (x, y) = (c, r)
-  while x < m[0].len - 1: # E
-    x.inc()
-    register(x, y)
-  (x, y) = (c, r)
-  while y < m.len - 1 and x < m[0].len - 1: # SE
-    y.inc()
-    x.inc()
-    register(x, y)
-  (x, y) = (c, r)
-  while y < m.len - 1: # S
-    y.inc()
-    register(x, y)
-  (x, y) = (c, r)
-  while y < m.len - 1 and x > 0: # SW
-    y.inc()
-    x.dec()
-    register(x, y)
-  (x, y) = (c, r)
-  while x > 0: #W
-    x.dec()
-    register(x, y)
-  (x, y) = (c, r)
-  while x > 0 and y > 0: # NW
-    x.dec()
-    y.dec()
-    register(x, y)
-  for cell in registered:
-    yield cell
+  for d in DIRS:
+    var (x, y) = (c, r)
+    while true:
+      y += d[0]
+      x += d[1]
+      if (y in 0..<m.len) and (x in 0..<m[0].len):
+        if m[y][x] != Floor:
+          yield m[y][x]
+          break
+      else:
+        break
 
-template scanNeighbours(m: openArray[seq[Cell]]; part: Part) =
+template scanNeighbours(part: Part) =
   let
     w = m[0].len
     h = m.len
@@ -83,30 +54,30 @@ template scanNeighbours(m: openArray[seq[Cell]]; part: Part) =
     for c in 0..<w:
       let seat = m[r][c]
       newrow[c] = if seat != Floor:
-        let occupied = when part == P1:
-            neighboursSafe(w, h, (c, r)) --> map(m[it[1]][it[0]]).
-                                              filter(it == Occ).count()
-        else:
-            toSeq(scanAround(m, r, c)).foldl(if b == Occ: a + 1 else: a, 0)
-        if seat == Empty and occupied == 0:
-          block:
-            changed = true
-            Occ
-        elif seat == Occ and (when part == P1: occupied >= 4 else: occupied >= 5):
-          block:
-            changed = true
-            Empty
-        else:
-          seat
-      else: Floor
+          let occupied = when part == P1:
+              neighboursSafe(w, h, (c, r)) --> map(m[it[1]][it[0]]).
+                                                filter(it == Occ).count()
+          else:
+              toSeq(scanAround(m, r, c)).foldl(if b == Occ: a + 1 else: a, 0)
+          if seat == Empty and occupied == 0:
+            block:
+              changed = true
+              Occ
+          elif seat == Occ and (when part == P1: occupied >= 4 else: occupied >= 5):
+            block:
+              changed = true
+              Empty
+          else:
+            seat
+        else: Floor
     result[1].add(newrow)
   result[0] = changed
 
 func stepStateP1(m: sink seq[seq[Cell]]): (bool, seq[seq[Cell]]) =
-  scanNeighbours(m, P1)
+  scanNeighbours(P1)
 
 func stepStateP2(m: sink seq[seq[Cell]]): (bool, seq[seq[Cell]]) =
-  scanNeighbours(m, P2)
+  scanNeighbours(P2)
 
 func countCell(m: seq[seq[Cell]], cell: Cell): int =
   m --> map( it --> filter(it == cell).count() ).sum()
